47ee3a3c2733a6903de2c89ddec698d8f5365428
vfunny2000@rambler.ru <vfunny2000@rambler.ru>
2022-08-09 19:14:56 +0300

    added Task with 3rd digit and 3rd is out

---
+++ Task013_3rd_Digit_and_out/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введи число: ");
+int anyNumber = Convert.ToInt32(Console.ReadLine());
+string anyNumberText = Convert.ToString(anyNumber);
+if (anyNumberText.Length > 2){
+  Console.WriteLine("третья цифра -> " + anyNumberText[2]);
+}
+else {
+  Console.WriteLine("-> третьей цифры нет");
+}
