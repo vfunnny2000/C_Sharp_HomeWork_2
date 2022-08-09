f0ef1a1b9c9ec4c11ac8b0e3a4da7c5689bbaa20
vfunny2000@rambler.ru <vfunny2000@rambler.ru>
2022-08-09 19:00:17 +0300

    added Task010 Second digit our of three

---
+++ Task010_Second_Digit/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.Write("Введи трёхзначное число: ");
+int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
+string stringNumber = Convert.ToString(threeDigitNumber);
+Console.WriteLine("вторая цифра этого числа -> "+stringNumber[1]);
