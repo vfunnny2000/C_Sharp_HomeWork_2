8e98da61812e729dfd7b3082e6d068ab225f9c12
vfunny2000@rambler.ru <vfunny2000@rambler.ru>
2022-08-13 21:55:19 +0300

    added home work

--- Task010_Second_Digit/Program.cs
+++ Task010_Second_Digit/Program.cs
@@ -1,4 +1,24 @@
-﻿Console.Write("Введи трёхзначное число: ");
+﻿/* Напишите программу, которая принимает на вход трёхзначное число и на
+выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1 */
+// 1-st variant
+
+Console.Write("Введи трёхзначное число: ");
 int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
 string stringNumber = Convert.ToString(threeDigitNumber);
-Console.WriteLine("вторая цифра этого числа -> "+stringNumber[1]);
+Console.WriteLine("вторая цифра этого числа -> "+stringNumber[1]); 
+
+
+/* 2-nd variant
+
+Console.WriteLine("Введите трехзначное число: ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+int secondNumber = (number % 100) / 10;
+
+Console.Write(secondNumber);
+ */
--- Task013_3rd_Digit_and_out/Program.cs
+++ Task013_3rd_Digit_and_out/Program.cs
@@ -1,9 +1,18 @@
-﻿Console.Write("Введи число: ");
+﻿/* Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6 */
+
+Console.Write("Введите число: ");
+
 int anyNumber = Convert.ToInt32(Console.ReadLine());
 string anyNumberText = Convert.ToString(anyNumber);
-if (anyNumberText.Length > 2){
-  Console.WriteLine("третья цифра -> " + anyNumberText[2]);
+
+if (anyNumberText.Length > 2)
+{
+    Console.WriteLine("третья цифра -> " + anyNumberText[2]);
 }
-else {
-  Console.WriteLine("-> третьей цифры нет");
+else 
+{
+    Console.WriteLine("-> третьей цифры нет");
 }
