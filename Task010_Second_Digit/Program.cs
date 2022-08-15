6f0abe13aa69227ef18d3ecbd3f6773e483e0c37
vfunny2000@rambler.ru <vfunny2000@rambler.ru>
2022-08-15 10:57:36 +0300

    2nd variant

--- Task010_Second_Digit/Program.cs
+++ Task010_Second_Digit/Program.cs
@@ -1,18 +1,17 @@
 ﻿/* Напишите программу, которая принимает на вход трёхзначное число и на
 выходе показывает вторую цифру этого числа.
-
 456 -> 5
 782 -> 8
 918 -> 1 */
-// 1-st variant
+
+/* 1-st variant */
 
 Console.Write("Введи трёхзначное число: ");
 int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
 string stringNumber = Convert.ToString(threeDigitNumber);
 Console.WriteLine("вторая цифра этого числа -> "+stringNumber[1]); 
 
-
-/* 2-nd variant
+/*  2-nd variant  */
 
 Console.WriteLine("Введите трехзначное число: ");
 
@@ -21,4 +20,4 @@ int number = Convert.ToInt32(Console.ReadLine());
 int secondNumber = (number % 100) / 10;
 
 Console.Write(secondNumber);
- */
+ 
