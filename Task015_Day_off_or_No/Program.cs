c8373020a4e1c259a4d45e33bbcb7b399880f3cb
vfunny2000@rambler.ru <vfunny2000@rambler.ru>
2022-08-15 10:55:53 +0300

    updates

--- Task015_Day_off_or_No/Program.cs
+++ Task015_Day_off_or_No/Program.cs
@@ -8,14 +8,56 @@
 Console.Write("Введи цифру, обозначающую день недели: ");
 int dayNumber = Convert.ToInt32(Console.ReadLine());
 
-void CheckingTheDayOfTheWeek (int dayNumber) {
+void CheckingTheDayOfTheWeek (int dayNumber) 
+{
   if (dayNumber == 6 || dayNumber == 7) {
   Console.WriteLine("этот день выходной");
   }
-  else if (dayNumber < 1 || dayNumber > 7) {
+  else if (dayNumber < 1 || dayNumber > 7) 
+  {
     Console.WriteLine("это не день недели");
   }
   else Console.WriteLine("этот день не выходной");
 }
 
 CheckingTheDayOfTheWeek(dayNumber);
+
+/* *************
+2nd variant
+********************* */ 
+Console.WriteLine("Введите цифру, обозначающую день недели: ");
+try
+{
+    int a = Convert.ToInt32(Console.ReadLine());
+    switch (a)
+    {
+        case 1:
+            Console.WriteLine("Понедельник");
+            break;
+        case 2:
+            Console.WriteLine("Вторник");
+            break;
+        case 3:
+            Console.WriteLine("Среда");
+            break;
+        case 4:
+            Console.WriteLine("Четверг");
+            break;
+        case 5:
+            Console.WriteLine("Пятница");
+            break;
+        case 6:
+            Console.WriteLine("Суббота");
+            break;
+        case 7:
+            Console.WriteLine("Воскресенье");
+            break;
+        default:
+            Console.WriteLine("Такого дня недели нет");
+            break;
+    }
+}
+catch (FormatException)
+{
+    Console.WriteLine();
+}
